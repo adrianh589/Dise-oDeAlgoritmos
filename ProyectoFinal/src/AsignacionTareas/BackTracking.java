f450681fa77ffad390d9b0857bff1e20baebc0a4
adrian hoyos <adrianh_589@hotmail.com>
2019-11-17 14:07:25 -0500

    Terminado, falta menu

--- ProyectoFinal/src/AsignacionTareas/BackTracking.java
+++ ProyectoFinal/src/AsignacionTareas/BackTracking.java
@@ -1,160 +1,135 @@
-package AsignacionTareas;
-
-import java.util.ArrayList;
 import java.util.Stack;
 
-/**
- * Clase BackTracking que se emplea para la asignacion de tareas
- * @author Adrian Hoyos
- *
- */
 public class BackTracking {
 
-    
-    static int sumaActual = 0;
     static int minimoCoste = Integer.MAX_VALUE;
-    static ArrayList<int[][]> solucionFinal= new ArrayList<int[][]>();
+    public static int[][] solucionFinal ;
 
     /**
-     * Metodo para realizar el backtracking
+     * Metodo backtracking
      * @param tablero
      * @param persona
-     * @param tarea
-     * @param bloquearPersona
+     * @param conteo
      * @param bloquearTarea
-     * @param solucionParcial
+     * @return
      */
-    public static void backtracking( int[][] tablero, int persona, int tarea, Stack<Integer> bloquearPersona, Stack<Integer> bloquearTarea, int[][] solucionParcial ) {
+    static boolean VueltaAtras(int[][] tablero, int persona, int conteo, Stack<Integer> bloquearTarea, int[][] solucion){
 
-        if (esSolucion(solucionParcial)) {//Caso base
-            solucionFinal.add(clonarMatriz(solucionParcial));//Añadimos esa solucion como postulante
-            imprimir(solucionParcial);//Imprimimos la solucion parcial
-            System.out.println("Esta solucion por el momento es la mas optima con un coste de: "+darCoste(solucionParcial)+"\n");
-            minimoCoste = sumaActual;//El minimo coste ahora es la solucion encontrada
-            sumaActual -= solucionParcial[bloquearPersona.peek()][bloquearTarea.peek()];//Reducimos el coste actual puesto que nos estamos devolviendo
-            solucionParcial[bloquearPersona.pop()][bloquearTarea.pop()] = 0;//Eliminamos la solucion parcial
-            return;
-        } else {//Procedimiento
+        if(esSolucion(solucion)){
+            minimoCoste = conteo;
+            solucionFinal = solucion;
+            imprimir(solucion);
+            System.out.println("Costo minimo encontrado: "+conteo+" \n");
+            return false;//Al llegar al final nos devolvemos
+        }else{
+            for (int tarea = 0; tarea < tablero.length; tarea++) {
 
-                for (int j = 0; j < tablero.length; j++) {//Nos enfocamos en las tareas
-                    if (!(bloquearTarea.contains(j))) {//Si la tarea y la persona no estan bloqueadas
-                        bloquearTarea.add(j); bloquearPersona.add(persona);//La persona es necesaria para eliminar la tarea
-                        solucionParcial[persona][j] = tablero[persona][j];//Armamos nuestra solucion parcial
-                        sumaActual += tablero[persona][j];
-                        if(sumaActual > minimoCoste){//Si la suma actual es mayor que el minimo coste encontrado, no se va por esa via
-                            imprimir(solucionParcial);
-                            System.out.println("Esta via no es optima, por lo tanto se hara vuelta atras puesto que el coste minimo actual es de "+minimoCoste+" y esta via representa un coste de "+darCoste(solucionParcial)+"\n");
-                            sumaActual -= solucionParcial[bloquearPersona.pop()][bloquearTarea.pop()];
-                            solucionParcial[persona][j] = 0;
-                        }else {
-                            backtracking(tablero, persona + 1, j, bloquearPersona, bloquearTarea, solucionParcial);//Si la suma actual es optima, nos vamos por esa via
+                if(!bloquearTarea.contains(tarea)){
+                    bloquearTarea.add(tarea);//Bloqueamos la tarea
+                    conteo += tablero[persona][tarea];//sumamos el conteo
+                    solucion[persona][tarea] = tablero[persona][tarea];//mantenemos una solucion parcial
+                    imprimir(solucion);
+
+                        if(conteo > minimoCoste){//Nos devolvemos si el costo supera al minimo
+                            System.out.println("El costo de esta via es "+conteo+" y el minimo costo es de "+minimoCoste+" se hara vuelta atras");
+                            imprimir(solucion);
+                            if(tarea == tablero.length-1) {//Si hay mas tareas disponibles no podemos devolvernos todavia
+                                return false;
+                            }else{//En caso de que hayan tareas disponibles, eliminamos lo temporal y vamos a preguntar por la siguiente tarea
+                                solucion[persona][tarea] = 0; conteo -= tablero[persona][tarea]; bloquearTarea.pop();
+                                imprimir(solucion);
+                                continue;//Se pregunta por la siguiente tarea
+                            }
+                        }else {//Si no ha superado el minimo, podemos seguir asignando tareas
+                            //Aqui es donde se indica que vamos a avanzar, se observa que se llama la misma funcion pero con persona + 1
+                            VueltaAtras(tablero, persona+1, conteo, recordarTareas(bloquearTarea), solucionActual(solucion));
+                            //Borramos lo que llevamos
+                                conteo -= tablero[persona][tarea]; bloquearTarea.pop(); solucion[persona][tarea] = 0; imprimir(solucion);
                         }
-                    }
                 }
-                if (!bloquearPersona.isEmpty()){//Al devolvernos hasta la primera persona, debemos tener en cuenta que la tarea que le asignaremos es la que le sigue
-                    sumaActual -= solucionParcial[bloquearPersona.peek()][bloquearTarea.peek()];
-                    solucionParcial[bloquearPersona.pop()][bloquearTarea.pop()] = 0;
-                }
+            }
+            return false;//Si no hay mas tareas disponibles nos devolvemos
         }
     }
 
     /**
-     * Verificar si es la solucion
+     * Metodo para evitar que las tareas sean las mismas para todos los nodos
+     * @param bloquearTareas
+     * @return
+     */
+    static Stack<Integer> recordarTareas(Stack<Integer> bloquearTareas){
+        Stack<Integer> recordar = new Stack<Integer>();
+
+        for (int i = 0; i < bloquearTareas.size(); i++) {
+            recordar.add(bloquearTareas.get(i));
+        }
+
+        return recordar;
+    }
+
+    /**
+     * Guardamos la solucion que lleva
+     * @param solucionParcial
+     * @return
+     */
+    static int[][] solucionActual(int[][] solucionParcial){
+        int[][] solucion = new int[solucionParcial.length][solucionParcial.length];
+        for (int i = 0; i < solucionParcial.length; i++) {
+            for (int j = 0; j < solucionParcial.length; j++) {
+                solucion[i][j] = solucionParcial[i][j];
+            }
+        }
+        return solucion;
+    }
+
+    /**
+     * Metodo para comprobar si se llego a una solucion
      * @param tablero
      * @return
      */
-    static boolean esSolucion(int [][] tablero){
-        int elementos = 0;
+    static boolean esSolucion(int[][] tablero){
+        int counter = 0;
         for (int i = 0; i < tablero.length; i++) {
             for (int j = 0; j < tablero.length; j++) {
                 if(tablero[i][j] != 0){
-                    elementos+=1;
+                    counter+=1;
                 }
             }
         }
-
-        if (elementos == tablero.length){
+        if(counter == tablero.length){
             return true;
         }else{
-            return false;
+            return  false;
         }
+
     }
 
     /**
-     * MAtriz que dice el cost de una posible solucion
-     * @param solucionParcial
-     * @return
-     */
-    static int darCoste(int[][] solucionParcial){
-        int coste = 0;
-        for (int i = 0; i < solucionParcial.length; i++) {
-            for (int j = 0; j < solucionParcial.length; j++) {
-                if(solucionParcial[i][j] != 0){
-                    coste+= solucionParcial[i][j];
-                }
-            }
-        }
-        return coste;
-    }
-
-    /**
-     * Metodo para imprimir la solucion encontrada
+     * MEtodo para imprimir los pasos de la solucion
      * @param solucion
      */
-    public static void imprimir(int[][] solucion){
-
-            for (int i = 0; i < solucion.length; i++) {
-                for (int j = 0; j < solucion.length; j++) {
-                    System.out.print(solucion[i][j]+" ");
-                }
-                System.out.println();
-            }
-
-    }
-
-    /**
-     * Metodo para clonar la matriz, debido a que se modifica
-     * @param solucion
-     * @return
-     */
-    static int[][] clonarMatriz(int[][] solucion){
-        int[][] aux = new int [solucion.length][solucion.length];
-
+    static void imprimir(int[][] solucion){
         for (int i = 0; i < solucion.length; i++) {
             for (int j = 0; j < solucion.length; j++) {
-                aux[i][j] = solucion[i][j];
+                System.out.print(solucion[i][j] + " ");
             }
+            System.out.println("");
         }
-
-        return aux;
-
+        System.out.println();
     }
 
     /**
-     * Metodo que dice las posibles soluciones dependiendo del coste minimo
-     * @param solucionesFinales
+     * Metodo que dira que tarea debe elegir cada persona para reducir tiempos
+     * @param asignador
      */
-    public static void darPosiblesSoluciones(ArrayList<int[][]> solucionesFinales){
-
-        System.out.println("Las posibles soluciones segun el coste generado son: \n");
-
-        for (int i = 0; i < solucionesFinales.size(); i++) {
-
-            if(darCoste(solucionesFinales.get(i)) == minimoCoste) {
-
-                for (int j = 0; j < solucionesFinales.get(i).length; j++) {
-                    for (int k = 0; k < solucionesFinales.get(i).length; k++) {
-                        if(solucionesFinales.get(i)[j][k] != 0){
-                            System.out.println("persona "+(j+1)+" tarea "+(k+1));
-                        }
-                    }
+    public static void asignadorTareas(int[][] asignador){
+        for (int i = 0; i < asignador.length; i++) {
+            for (int j = 0; j < asignador.length; j++) {
+                if(asignador[i][j]!=0){
+                    System.out.println("Persona "+(i+1)+" tarea "+(j+1));
                 }
-
-                System.out.println();
             }
-
         }
-
     }
 
 }
